a65456ffc64f2f27730dd76efcf8ec1d55542771
ridaeh <ridaehamdani@gmail.com>
2021-11-30 22:32:54 +0100

    first init

--- pwa.js
+++ pwa.js
@@ -1,1 +1,16 @@
-console.log('hello');
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker
+      .register('https://cdn.jsdelivr.net/gh/ridaeh/pwa@master/pwa.js')
+      .then(
+        function(registration) {
+          // Registration was successful
+          console.log('ServiceWorker registration successful');
+        },
+        function(err) {
+          // registration failed :(
+          console.log('ServiceWorker registration failed: ', err);
+        }
+      );
+  });
+}
